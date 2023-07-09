7acfc45f379673472898ec3f47ac37469c7efc52
mhmmdbrcn <muhammedbircan25@gmail.com>
2023-07-09 19:03:53 +0300

    cart hesap işlemleri

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,6 @@
 let cart = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : [];
-
 function displayCartProduct() {
   const cartWrapper = document.querySelector(".cart-wrapper");
   let result = "";
@@ -11,13 +10,14 @@ function displayCartProduct() {
     <td></td>
     <td class="cart-image">
         <img src=${item.img.singleImage} alt="">
-    
         <i class="bi bi-x delete-cart" data-id=${item.id}></i>
     </td>
     <td>${item.name}</td>
-    <td>$${item.price.newPrice}</td>
+    <td>$${item.price.newPrice.toFixed(2)}</td>
     <td class="product-quantity">${item.quantity}</td>
-    <td class="product-subtotal">$108.00</td>
+    <td class="product-subtotal">${(
+      item.price.newPrice * item.quantity
+    ).toFixed(2)}</td>
    </tr>
    `;
   });
@@ -38,6 +38,31 @@ function removeCartItem() {
       displayCartProduct();
       localStorage.setItem("cart", JSON.stringify(cart));
       cartItems.innerHTML = cart.length;
+      saveCartValues();
     });
   });
 }
+
+function saveCartValues() {
+    const cartTotal = document.getElementById("cart-total");
+    const subtotal = document.getElementById("subtotal");
+    const fastCargo = document.getElementById("fast-cargo");
+    const fastCargoPrice = 15;
+    let itemsTotal = 0;
+  
+    cart.length > 0 &&
+      cart.map((item) => (itemsTotal += item.price.newPrice * item.quantity));
+    console.log(itemsTotal);
+    subtotal.innerHTML = `$${itemsTotal.toFixed(2)}`;
+    cartTotal.innerHTML = `$${itemsTotal.toFixed(2)}`;
+  
+    fastCargo.addEventListener("change", function (e) {
+      if (e.target.checked) {
+        cartTotal.innerHTML = `$${(itemsTotal + fastCargoPrice).toFixed(2)}`;
+      } else {
+        cartTotal.innerHTML = `$${itemsTotal.toFixed(2)}`;
+      }
+    });
+  }
+  
+  saveCartValues();
