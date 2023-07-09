75eca44e13e3e6a4e224e9673dad164d51b91258
mhmmdbrcn <muhammedbircan25@gmail.com>
2023-07-09 17:30:09 +0300

    ürün sepete ekleme fonskiyonu

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 import headerFunc from "./header.js";
 import productsFunc from "./products.js";
 
-//! add product to localStorage
-async function getData() {
+//! add product to localStorage start
+(async function () {
   const photos = await fetch("../js/data.json");
   const data = await photos.json();
 
   data ? localStorage.setItem("products", JSON.stringify(data)) : [];
-}
+  productsFunc();
+})();
+//! add product to localStorage end
 
-getData();
+//! add cartItems to localStorage start
+const cartItems = document.querySelector(".header-cart-count");
 
-const products = localStorage.getItem("products");
+cartItems.innerHTML = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart")).length
+  : "0";
+//! add cartItems to localStorage end
